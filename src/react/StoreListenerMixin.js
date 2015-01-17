47a622ee3061ed5a34830371631bd3da597e2d89
Colby Rabideau <crabideau@hubspot.com>
2015-01-17 00:36:32 -0500

    StoreListenerMixin for React

---
+++ src/react/StoreListenerMixin.js
@@ -0,0 +1,61 @@
+/* @flow */
+
+var StoreFacade = require('../store/StoreFacade.js');
+
+var StoreListenerMixin = {
+
+  componentWillMount(): void {
+    if (typeof this.getStoreState !== 'function') {
+      throw new Error(
+        'StoreListenerMixin: expected this.getStoreState to be a function.'
+      )
+    }
+    if (!Array.isArray(this.stores)) {
+      throw new Error(
+        'StoreListenerMixin: this.stores must be an array of stores.'
+      );
+    }
+    if (this.stores.length < 1) {
+      throw new Error(
+        'StoreListenerMixin: no stores are defined in this.stores.'
+      );
+    }
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    this.stores.forEach(
+      store => store.addOnChange(this.handleStoreChange)
+    );
+    this.handleStoreChange();
+  },
+
+  componentWillUnmount(): void {
+    this.stores.forEach(
+      store => store.removeOnChange(this._handleStoreChange)
+    );
+  },
+
+  handleStoreChange(): void {
+    this.setState(
+      this.getStoreState()
+    );
+  }
+
+  /**
+   * // an array of StoreFacades
+   * stores: [
+   *   ExampleStore
+   * ],
+   */
+
+  /**
+   * // the return value of getStoreState will be merged
+   * // into the component state
+   * getStoreState(): Object {
+   *   return {
+   *     exampleData: ExampleStore.get()
+   *   };
+   * }
+   */
+
+};
+
+module.exports = StoreListenerMixin;
