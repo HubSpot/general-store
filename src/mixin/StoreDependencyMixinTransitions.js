1cd0465d6a063fe8541cf8c1827d748ba0a9e28a
Colby Rabideau <crabideau@hubspot.com>
2015-03-02 14:50:08 -0500

    test StoreDependencyMixinTransitions

--- src/mixin/StoreDependencyMixinTransitions.js
+++ src/mixin/StoreDependencyMixinTransitions.js
@@ -2,14 +2,49 @@
  * @flow
  */
 
+var compare = (window.Immutable && typeof window.Immutable.is === 'function') ?
+  window.Immutable.is :
+  function(a, b) {return a === b;};
+
+function compareKey(key, objA, objB) {
+  return compare(objA[key], objB[key]);
+}
+
+function shallowEqual(
+  is: (key: string, objA: Object, objB: Object) => bool,
+  objA: Object,
+  objB: Object
+): bool {
+  if (objA === objB) {
+    return true;
+  }
+  var key;
+  // Test for A's keys different from B.
+  for (key in objA) {
+    if (objA.hasOwnProperty(key) &&
+        (!objB.hasOwnProperty(key) || !is(key, objA, objB))) {
+      return false;
+    }
+  }
+  // Test for B's keys missing from A.
+  for (key in objB) {
+    if (objB.hasOwnProperty(key) && !objA.hasOwnProperty(key)) {
+      return false;
+    }
+  }
+  return true;
+}
+
 var StoreDependencyMixinTransitions = {
   hasPropsChanged(
     oldProps: Object,
     nextProps: Object
   ): bool {
-    return Object
-      .keys(nextProps)
-      .some(key => oldProps[key] !== nextProps[key]);
+    return !shallowEqual(
+      compareKey,
+      oldProps,
+      nextProps
+    );
   },
 
   hasStateChanged(
@@ -17,11 +52,16 @@ var StoreDependencyMixinTransitions = {
     oldState: Object,
     nextState: Object
   ): bool {
-    return Object
-      .keys(nextState)
-      .some(
-        key => !stores.hasOwnProperty(key) && oldState[key] !== nextState[key]
-      );
+    return !shallowEqual(
+      (key, objA, objB) => {
+        return ( // if the value is a store, ignore it
+          stores.hasOwnProperty(key) ||
+          compare(objA[key], objB[key])
+        );
+      },
+      oldState,
+      nextState
+    );
   },
 
   mergeState(state: Object, updates: Object): Object {
