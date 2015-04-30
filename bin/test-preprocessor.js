a950bced90de5a577af448c85d365a925209569d
Colby Rabideau <crabideau@hubspot.com>
2015-04-30 02:16:56 -0400

    upgrading and linting

--- bin/test-preprocessor.js
+++ bin/test-preprocessor.js
@@ -1,19 +1,19 @@
-var babel = require("babel-core");
+var babel = require('babel');
 
 module.exports = {
   process: function (src, filename) {
     // Allow the stage to be configured by an environment
     // variable, but use Babel's default stage (2) if
     // no environment variable is specified.
-    var stage = process.env.BABEL_JEST_STAGE || 2
+    var stage = process.env.BABEL_JEST_STAGE || 2;
 
     // Ignore all files within node_modules
     // babel files can be .js, .es, .jsx or .es6
-    if (filename.indexOf("node_modules") === -1 && babel.canCompile(filename)) {
+    if (filename.indexOf('node_modules') === -1 && babel.canCompile(filename)) {
       return babel.transform(src, {
         filename: filename,
         stage: stage,
-        loose: ["es6.classes"],
+        loose: ['es6.classes'],
       }).code;
     }
     return src;
