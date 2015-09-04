a7ac7586352b3867752ab2735bea6c2b91530508
Colby Rabideau <crabideau@hubspot.com>
2015-09-04 12:47:44 -0400

    avoid "define" fn conflict with ES6 imports

--- src/GeneralStore.js
+++ src/GeneralStore.js
@@ -4,9 +4,12 @@ import StoreDefinition from './store/StoreDefinition.js';
 import _DispatcherInstance from './dispatcher/DispatcherInstance.js';
 import _StoreDependencyMixin from './mixin/StoreDependencyMixin.js';
 
-export function define(): StoreDefinition {
+function define(): StoreDefinition {
   return new StoreDefinition();
 }
-export var DispatcherInstance = _DispatcherInstance;
-export var StoreDependencyMixin = _StoreDependencyMixin;
+export default {
+  define: define,
+  DispatcherInstance: _DispatcherInstance,
+  StoreDependencyMixin: _StoreDependencyMixin,
+};
 
