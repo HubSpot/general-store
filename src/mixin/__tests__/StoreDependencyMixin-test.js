28971d83a643d0664eccf98f858208bbc5ca9e9c
Colby Rabideau <idontknow5691@gmail.com>
2016-07-04 11:45:40 -0400

    spec out an updated StoreDependencyMixin-test

--- src/mixin/__tests__/StoreDependencyMixin-test.js
+++ src/mixin/__tests__/StoreDependencyMixin-test.js
@@ -1,62 +1,33 @@
 jest.dontMock('../StoreDependencyMixin.js');
 
 describe('StoreDependencyMixin', () => {
-
   let StoreDependencyMixin;
-
-  let mockComponent;
-  let mockMixin;
 
   beforeEach(() => {
     StoreDependencyMixin = require('../StoreDependencyMixin.js');
-
-    mockComponent = {
-      props: {},
-      state: {},
-      setState: jest.genMockFn(),
-    };
-    mockMixin = StoreDependencyMixin({});
-    mockMixin.getInitialState.call(mockComponent);
   });
 
-  it('applies dependencies in getInitialState', () => {
-    let {applyDependencies} = require('../StoreDependencyMixinInitialize.js');
-    let {getDependencyState} = require('../StoreDependencyMixinState.js');
-    expect(applyDependencies.mock.calls.length).toBe(1);
-    expect(getDependencyState.mock.calls.length).toBe(1);
+  describe('propTypes', () => {
+    it('has propTypes if a dependency specifices them');
   });
 
-  it('sets handlers in componentWillMount', () => {
-    let {setupHandlers} = require('../StoreDependencyMixinHandlers.js');
-    mockMixin.componentWillMount.call(mockComponent);
-    expect(setupHandlers.mock.calls.length).toBe(1);
+  describe('componentWillMount', () => {
+    it('registers a callback with the dispatcher');
+
+    it('calculates and sets initial state');
   });
 
-  it('cleans up handlers in componentWillUnmount', () => {
-    let {cleanupHandlers} = require('../StoreDependencyMixinHandlers.js');
-    mockMixin.componentWillUnmount.call(mockComponent);
-    expect(cleanupHandlers.mock.calls.length).toBe(1);
+  describe('componentWillReceiveProps', () => {
+    it('calculates and sets state');
+
+    it('doesnt recalculate fields that dont use props');
   });
 
-  it('sets state if appropriate in componentWillReceiveProps', () => {
-    let {hasPropsChanged} = require('../StoreDependencyMixinTransitions.js');
-    hasPropsChanged.mockReturnValueOnce(false);
-    mockMixin.componentWillReceiveProps.call(mockComponent);
-    expect(mockComponent.setState.mock.calls.length).toBe(0);
-    hasPropsChanged.mockReturnValueOnce(true);
-    mockMixin.componentWillReceiveProps.call(mockComponent);
-    expect(mockComponent.setState.mock.calls.length).toBe(1);
+  describe('componentDidUpdate', () => {
+    it('has a componentDidUpdate if a field uses state');
   });
 
-  it('sets state if appropriate in componentDidUpdate', () => {
-    let {hasStateChanged} = require('../StoreDependencyMixinTransitions.js');
-    hasStateChanged.mockReturnValueOnce(false);
-    mockMixin.componentDidUpdate.call(mockComponent);
-    expect(mockComponent.setState.mock.calls.length).toBe(0);
-    hasStateChanged.mockReturnValueOnce(true);
-    mockMixin.componentDidUpdate.call(mockComponent);
-    expect(mockComponent.setState.mock.calls.length).toBe(1);
+  describe('componentWillUnmount', () => {
+    it('unregisters its dispatcher callback');
   });
-
 });
-
