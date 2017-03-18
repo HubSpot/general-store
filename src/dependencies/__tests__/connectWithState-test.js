eda6797fc8161e92766bcbf17d5971faac007543
Colby Rabideau <crabideau@hubspot.com>
2017-03-18 14:38:13 -0400

    connectWithState

---
+++ src/dependencies/__tests__/connectWithState-test.js
@@ -0,0 +1,125 @@
+jest.disableAutomock();
+import connectWithState from '../connectWithState';
+import {Dispatcher} from 'flux';
+import {mount, shallow} from 'enzyme';
+import React from 'react';
+import StoreFactory from '../../store/StoreFactory';
+
+function BaseComponent() {
+  return <div />;
+}
+
+BaseComponent.displayName = 'BaseComponent';
+BaseComponent.testStaticMethod = () => true;
+
+describe('connect', () => {
+  const defaultInitialState = {testing: true};
+
+  const FIRST_ONLY = 'FIRST_ONLY';
+  const SHARED = 'SHARED';
+  const FirstStoreFactory = new StoreFactory({})
+    .defineGet(state => state)
+    .defineGetInitialState(() => 1)
+    .defineResponses({
+      [FIRST_ONLY]: state => state + 1,
+      [SHARED]: state => state - 1,
+    });
+
+  let dispatcher;
+  let dependencies;
+  let FirstStore;
+  let MockComponent;
+
+  beforeEach(() => {
+    dispatcher = new Dispatcher();
+    dispatcher.register = jest.fn(dispatcher.register);
+    dispatcher.unregister = jest.fn(dispatcher.unregister);
+    dispatcher.waitFor = jest.fn(dispatcher.waitFor);
+    FirstStore = FirstStoreFactory.register(dispatcher);
+    dependencies = {
+      one: {
+        stores: [FirstStore],
+        deref: () => FirstStore.get(),
+      },
+    };
+    MockComponent = connectWithState(
+      defaultInitialState,
+      dependencies,
+      dispatcher
+    )(BaseComponent);
+  });
+
+  describe('statics', () => {
+    it('exports dependencies', () => {
+      expect(MockComponent.dependencies).toEqual(dependencies);
+    });
+
+    it('exports WrappedComponent', () => {
+      expect(MockComponent.WrappedComponent).toEqual(BaseComponent);
+    });
+
+    it('generates a proper displayName', () => {
+      expect(MockComponent.displayName).toBe(
+        'Stateful(Connected(BaseComponent))'
+      );
+    });
+
+    it('passes any statics through to ConnectedComponent', () => {
+      expect(typeof MockComponent.testStaticMethod).toBe('function');
+      expect(MockComponent.testStaticMethod).toBe(
+        BaseComponent.testStaticMethod
+      );
+    });
+  });
+
+  describe('focus', () => {
+    it('is undefined if BaseComponent has no focus method', () => {
+      expect(MockComponent.focus).toBe(undefined);
+    });
+
+    it('calls through to the BaseComponents focus', () => {
+      const focusSpy = jest.fn();
+      class ComponentWithFocus extends React.Component {
+        focus(...args) {
+          return focusSpy(...args);
+        }
+
+        render() {
+          return <div />;
+        }
+      }
+      const ConnectedComponentWithFocus = connectWithState(
+        defaultInitialState,
+        dependencies,
+        dispatcher
+      )(ComponentWithFocus);
+      const rendered = mount(<ConnectedComponentWithFocus />);
+      const connectedInstance = rendered.instance();
+      connectedInstance.focus('test', 123);
+      expect(focusSpy.mock.calls.length).toBe(1);
+      expect(focusSpy.mock.calls[0]).toEqual(['test', 123]);
+    });
+  });
+
+  describe('initialState', () => {
+    it('passes defaultInitialState if no initialState is specified', () => {
+      const rendered = shallow(<MockComponent />);
+      expect(rendered.prop('initialState')).toEqual(defaultInitialState);
+    });
+
+    it('passes initialState if it is specified', () => {
+      const initialState = {testing: false};
+      const rendered = shallow(<MockComponent initialState={initialState} />);
+      expect(rendered.prop('initialState')).toEqual(initialState);
+    });
+  });
+
+  describe('state', () => {
+    it('updates state', () => {
+      const rendered = shallow(<MockComponent />);
+      expect(rendered.prop('state')).toEqual(defaultInitialState);
+      rendered.prop('setState')({testing: false});
+      expect(rendered.prop('state')).toEqual({testing: false});
+    });
+  });
+});
---
+++ src/dependencies/connectWithState.js
@@ -0,0 +1,75 @@
+// @flow
+import {
+  makeDisplayName,
+  focuser,
+  transferNonReactStatics,
+} from './BuildComponent';
+import connect from './connect';
+import type {DependencyMap} from './DependencyMap';
+import {get as getDispatcherInstance} from '../dispatcher/DispatcherInstance';
+import type {Dispatcher} from 'flux';
+import React, {Component, PropTypes} from 'react';
+
+/* global ReactClass */
+function connector(
+  defaultInitialState: Object,
+  dependencies: DependencyMap,
+  depConnector: (BaseComponent: ReactClass<*>) => ReactClass<*>,
+  BaseComponent: ReactClass<*>
+) {
+  const ConnectedComponent = depConnector(BaseComponent);
+  class ConnectedComponentWithState extends Component {
+    static displayName = makeDisplayName('Stateful', ConnectedComponent);
+    static propTypes = {
+      ...ConnectedComponent.propTypes,
+      setState: undefined,
+      state: undefined,
+      initialState: PropTypes.object,
+    };
+    static WrappedComponent = BaseComponent;
+    static wrappedInstance: ?Object = null;
+
+    state = this.props.initialState || defaultInitialState;
+    wrappedInstance: ?Object;
+
+    boundSetState = this.setState.bind(this);
+
+    focus = typeof BaseComponent.prototype.focus === 'function'
+      ? (...args) => focuser(this, ...args)
+      : undefined;
+
+    setWrappedInstance = ref => {
+      this.wrappedInstance = ref;
+    };
+
+    render() {
+      const {initialState, ...props} = this.props;
+      return (
+        <ConnectedComponent
+          {...props}
+          initialState={initialState || defaultInitialState}
+          ref={this.setWrappedInstance}
+          setState={this.boundSetState}
+          state={this.state}
+        />
+      );
+    }
+  }
+
+  transferNonReactStatics(ConnectedComponent, ConnectedComponentWithState);
+
+  return ConnectedComponentWithState;
+}
+
+export default function connectWithState(
+  defaultInitialState: Object = {},
+  dependencies: DependencyMap,
+  dispatcher: ?Dispatcher = getDispatcherInstance()
+) {
+  return connector.bind(
+    null,
+    defaultInitialState,
+    dependencies,
+    connect(dependencies, dispatcher)
+  );
+}
